7bfc154d4c249404cea45cc72e3545512bb2a804
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-04-20 23:38:56 +0100

    todo

--- src/main/java/irc/IrcMain.java
+++ src/main/java/irc/IrcMain.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class IrcMain {
+    //TODO: COMMENTS
 
     // =============================================================================
     // FIELDS
